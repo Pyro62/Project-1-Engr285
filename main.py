fecc48e8d0263f4e8407ac31f18be21ced4b3723
PieRoe <ethanluu0508@gmail.com>
2026-04-06 14:42:28 -0700

    finished automating still need to patch name bug

--- main.py
+++ main.py
@@ -15,9 +15,19 @@ runs = data["runs"]
 script_dir = Path(__file__).parent
 
 for run in runs:
-    command = [sys.executable, "SnF.py", str(run["breed_time"]),str(run["energy_gain"]),str(run["breed_energy"]),str(run["dims_x"]),
-               str(run["dims_y"]),str(run["initial_fish"]),str(run["initial_sharks"]),str(run["steps"]),str(run["basicSetup"])]
-    subprocess.run(command, cwd=script_dir)
+    for i in range(run["attempts"]):
+        changes = run["changesPerAttempt"][0]
+        command = [sys.executable, "SnF.py",
+                    str(run["breed_time"]+(i*changes["d_breed_time"])),
+                    str(run["energy_gain"]+(i*changes["d_energy_gain"])),
+                    str(run["breed_energy"]+(i*changes["d_breed_energy"])),
+                    str(run["dims_x"]+(i*changes["d_dims_x"])),
+                    str(run["dims_y"]+(i*changes["d_dims_y"])),
+                    str(run["initial_fish"]+(i*changes["d_initial_fish"])),
+                    str(run["initial_sharks"]+(i*changes["d_initial_sharks"])),
+                    str(run["steps"]+(i*changes["d_steps"])),
+                    str(run["basicSetup"])]
+        subprocess.run(command, cwd=script_dir)
 
 
 
