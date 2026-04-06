d7236d961a1dff9984d47c987c5f360388cf6d79
PieRoe <ethanluu0508@gmail.com>
2026-04-06 13:16:19 -0700

    automated running stuff once

--- main.py
+++ main.py
@@ -6,14 +6,20 @@ config_path = Path(__file__).parent / "configs.json"
 try:
     with open(config_path, 'r') as file:
         data = json.load(file)
-    print("File data =", data)
+    #print("File data =", data)
     
 except json.JSONDecodeError:
     print("Error: Failed to decode JSON from the file.")
 
-
+runs = data["runs"]
 script_dir = Path(__file__).parent
-command = [sys.executable, "test.py", "3"]
 
-subprocess.run(command, cwd=script_dir)
+for run in runs:
+    command = [sys.executable, "SnF.py", str(run["breed_time"]),str(run["energy_gain"]),str(run["breed_energy"]),str(run["dims_x"]),
+               str(run["dims_y"]),str(run["initial_fish"]),str(run["initial_sharks"]),str(run["steps"]),str(run["basicSetup"])]
+    subprocess.run(command, cwd=script_dir)
+
+
+
+
 
